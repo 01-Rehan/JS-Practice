fb3a4a1921c1bccb9fc0324e73b5d76f2ccf98fd
Rehan Halai <halairehan01@gmail.com>
2025-07-18 12:10:21 +0530

    added search feature
    add search feature and made the webpage more responsive using the grid

--- moviesApp/script.js
+++ moviesApp/script.js
@@ -1,5 +1,8 @@
 import axios  from 'https://cdn.skypack.dev/axios';
 const api = "c083e2f95a6317ae05910d55cec56315";
+const searchWord = document.querySelector(".search");
+let allmovies;
+
 async function fetchMovieData () {
     try{
         let { data } = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${api}`);
@@ -9,53 +12,80 @@ async function fetchMovieData () {
     }
 }
 
-const promise = fetchMovieData();
-console.log(promise);
-    function create(element){
-         return document.createElement(element)
+async function initializeApp() {
+    try{
+        const movieData = await fetchMovieData();
+        if(movieData && movieData.results){
+            allmovies = movieData.results;
+            createCard(allmovies);
+        }
+
+    }catch(err){
+        console.log("initializing error");
     }
+}
+
+function create(element){
+    return document.createElement(element)
+}
 
 const container = document.querySelector(".container");
 
-promise.then(data =>{
-    data.results.forEach(movie =>{
 
-        let card = create("div");
-        card.classList.add("card");
-        
-        let imgCon = create("div");
-        imgCon.classList.add("movie-img");
-        let img = create("img");
-        img.setAttribute("src", `https://image.tmdb.org/t/p/original/${movie.poster_path}`);
-        imgCon.appendChild(img);
-        card.appendChild(imgCon);
+function createCard(MovieArray){
+    container.innerText = '';
 
-        let infoCon = create("div");
-        infoCon.classList.add("info");
+    MovieArray.forEach(movie =>{
 
-            let heading = create("p");
-            heading.classList.add("heading");
-            heading.innerText = movie.original_title;
-            infoCon.appendChild(heading);
-        
-            let language = create("p");
-            language.innerText = movie.original_language;
-            infoCon.appendChild(language);
+            let card = create("div");
+            card.classList.add("card");
+            
+            let imgCon = create("div");
+            imgCon.classList.add("movie-img");
+            let img = create("img");
+            img.setAttribute("src", `https://image.tmdb.org/t/p/original/${movie.poster_path}`);
+            imgCon.appendChild(img);
+            card.appendChild(imgCon);
+    
+            let infoCon = create("div");
+            infoCon.classList.add("info");
+    
+                let heading = create("p");
+                heading.classList.add("heading");
+                heading.innerText = movie.original_title;
+                infoCon.appendChild(heading);
+            
+                let language = create("p");
+                language.innerText = `Language : ${movie.original_language}`;
+                infoCon.appendChild(language);
+    
+                let date = create("p");
+                date.innerText = `Release Date : ${movie.release_date}`;
+                infoCon.appendChild(date);
+    
+                let rate = create("p");
+                rate.classList.add("description");
+                rate.innerText = `Rating : ${movie.vote_average}`;
+                infoCon.appendChild(rate);
+    
+                card.appendChild(infoCon);        
+            container.appendChild(card);
+        })    
+}
 
-            let date = create("p");
-            date.innerText = movie.release_date;
-            infoCon.appendChild(date);
+function SearchHandler(event,element){
+    let searchTerm = element.value.toLowerCase().trim();
 
-            let rate = create("p");
-            rate.classList.add("description");
-            rate.innerText = movie.vote_average;
-            infoCon.appendChild(rate);
+    let filterdArray;
+    if(searchTerm.length > 0)
+        filterdArray = allmovies.filter(movie =>  movie.original_title.toLowerCase().includes(searchTerm)) ;
+    else
+        filterdArray = allmovies;
+    
+    createCard(filterdArray);
+}
 
-            card.appendChild(infoCon);        
-        container.appendChild(card);
-    })
-})    
-const cards = document.querySelector(".card");
-document.addEventListener('onmouse',function hover(){
-    cards.setStyle.scale = 1.05;
-})
+searchWord.addEventListener("keyup", function (event){
+  SearchHandler(event,this) ; 
+} );
+initializeApp();
